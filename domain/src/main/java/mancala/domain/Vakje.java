45a261c03687a0e30655df96afaa99256f0ee040
Hamid Rhezouani <hrhezouani@sogyo.nl>
2022-04-25 11:33:06 +0200

    Vakje

---
+++ domain/src/main/java/mancala/domain/Vakje.java
@@ -0,0 +1,10 @@
+package main.java.mancala.domain;
+public class Vakje{
+    protected int nrOfBalls;
+    public Vakje(){
+        this.nrOfBalls = 4;
+    }
+    public int getNrOfBalls(){
+        return nrOfBalls;
+    }
+}
