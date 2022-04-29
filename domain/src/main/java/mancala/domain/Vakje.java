1c9bcb86b44ebc2a31cc6ed7667ce7042f8f3446
Hamid Rhezouani <hrhezouani@sogyo.nl>
2022-04-29 15:06:44 +0200

    Vakje commit

--- domain/src/main/java/mancala/domain/Vakje.java
+++ domain/src/main/java/mancala/domain/Vakje.java
@@ -3,11 +3,21 @@ package mancala.domain;
 import java.time.ZonedDateTime;
 
 public class Vakje{
+
+
+
     protected int nrOfBalls;
+
     protected Vakje firstVakje;
+
     protected Vakje nextVakje;
+
     protected Speler speler;
+
     protected boolean kalaha;
+
+
+
     public Vakje(Speler speler){
         this.nrOfBalls = 4;
         this.firstVakje = this;
@@ -15,46 +25,64 @@ public class Vakje{
         this.kalaha = false;
         this.nextVakje = new Vakje(this.speler, this.firstVakje, 1);
     }
+
     private Vakje(Speler speler, Vakje vakje, int nummer){
         this.firstVakje = vakje;
-        if (nummer == 6){
-            this.nrOfBalls = 0;
-            this.speler = speler;
-            this.kalaha = true;
-            nummer++;
-            this.nextVakje = new Vakje(this.speler.getTegenspeler() ,this.firstVakje, nummer);
-        }
-        else if (nummer == 13){
-            this.nrOfBalls = 0;
-            this.speler = speler;
-            this.kalaha = true;
-            this.nextVakje = vakje;
-        }
-        else{
-            this.nrOfBalls = 4;
-            this.speler = speler;
-            this.kalaha = false;
-            nummer++;
-            this.nextVakje = new Vakje(this.speler, this.firstVakje, nummer);
+        switch(nummer){
+            case 6:
+                this.nrOfBalls = 0;
+                this.speler = speler;
+                this.kalaha = true;
+                nummer++;
+                this.nextVakje = new Vakje(this.speler.getTegenspeler() ,this.firstVakje, nummer);
+                break;
+            case 13:
+                this.nrOfBalls = 0;
+                this.speler = speler;
+                this.kalaha = true;
+                this.nextVakje = vakje;
+                break;
+            default:
+                this.nrOfBalls = 4;
+                this.speler = speler;
+                this.kalaha = false;
+                nummer++;
+                this.nextVakje = new Vakje(this.speler, this.firstVakje, nummer);
         }
     }
+
     public int getNrOfBalls(){
         return nrOfBalls;
     }
+
     public Vakje getNextVakje(){
         return nextVakje;
     }
+
     public Speler getSpeler(){
         return speler;
     }
+
     public boolean getKalaha(){
         return kalaha;
     }
+
+    public Vakje getFirstVakje(){
+        return firstVakje;
+    }
+
+    public Vakje getVakje(int stappen){
+        if(stappen==0){
+            return this;
+        }
+        return this.getNextVakje().getVakje(stappen-1);
+    }
+    
     public int ballsOnPlayersSide(Speler speler){
         int countBalls = 0;
-        Vakje vak = this;
-        while (vak.getSpeler() != speler){
-            vak = vak.getNextVakje();
+        Vakje vak = this.getFirstVakje();
+        if (vak.getSpeler() != speler){
+            vak=vak.getVakje(7);
         }
         while (vak.getKalaha() == false){
             countBalls = countBalls + vak.getNrOfBalls();
@@ -62,26 +90,24 @@ public class Vakje{
         }
         return countBalls;
     }
+    
     public int ballsInKalaha(Speler speler){
-        Vakje vak = this;
-        while (vak.getSpeler() != speler){
-            vak = vak.getNextVakje();
+        Vakje vak = this.getFirstVakje();
+        if (vak.getSpeler() != speler){
+            vak = vak.getVakje(7);
         }
-        while (vak.getKalaha() == false){
-            vak = vak.getNextVakje();
-        }
+        vak = vak.getVakje(6);
         return vak.getNrOfBalls();
     }
+
     public void setNrOfBalls(int newNrOfBalls){
         nrOfBalls = newNrOfBalls;
     }
+
     public void addNrOfBalls(int additionalBalls){
         nrOfBalls = nrOfBalls + additionalBalls;
     }
-    public Vakje getVakje(int stappen){
-        if(stappen==0){
-            return this;
-        }
-        return this.getNextVakje().getVakje(stappen-1);
-    }
+
+
+    
 }
