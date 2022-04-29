a361c7c11c64a0124e3194e000913b651e897c8c
Hamid Rhezouani <hrhezouani@sogyo.nl>
2022-04-29 15:06:04 +0200

    Spel commit

--- domain/src/main/java/mancala/domain/Spel.java
+++ domain/src/main/java/mancala/domain/Spel.java
@@ -1,25 +1,38 @@
 package mancala.domain;
 public class Spel{
+
+
+
     protected Speler speler;
+
     protected Vakje vak;
+
+
+
     public Spel(){
         this.speler = new Speler();
         this.vak = new Vakje(this.speler);
         this.speler.setEersteVakje(this.vak);
         this.speler.getTegenspeler().setEersteVakje(this.vak.getVakje(7));
     }
+
     public void playRound(int choice){
         if(speler.getAanDeBeurt() == true){
-            speler.Distribute(choice, vak);
+            speler.distribute(choice, vak);
         }
         else{
-            speler.getTegenspeler().Distribute(choice,vak);
+            speler.getTegenspeler().distribute(choice,vak);
         }
     }
+
     public Speler getSpeler(){
         return speler;
     }
+
     public Vakje getVak(){
         return vak;
     }
+
+
+
 }
