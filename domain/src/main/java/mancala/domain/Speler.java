703744f8b6613c5a5a098c45a0487c17ef2eb85b
Hamid Rhezouani <hrhezouani@sogyo.nl>
2022-04-29 15:06:22 +0200

    Speler commit

--- domain/src/main/java/mancala/domain/Speler.java
+++ domain/src/main/java/mancala/domain/Speler.java
@@ -1,37 +1,49 @@
 package mancala.domain;
 public class Speler{
+
+
+
     protected boolean aanDeBeurt;
+
     protected Speler tegenspeler;
+
     protected Vakje eersteVakje;
+
+
 
     public Speler(){
         this.aanDeBeurt=true;
         this.tegenspeler = new Speler(this);
     }
+
     private Speler(Speler player){
         this.aanDeBeurt=false;
         this.tegenspeler=player;
     }
+
     public Speler getTegenspeler(){
         return tegenspeler;
     }
+
     public boolean getAanDeBeurt(){
         return aanDeBeurt;
     }
+
     public void setAanDeBeurt(boolean bool){
         aanDeBeurt = bool;
     }
+
     public Vakje chooseVakje(int choice, Vakje vakje){  //choice = 1,2,3,4,5 or 6
         choice--;
+        vakje = vakje.getFirstVakje();
         if (this != vakje.getSpeler()){
             choice = choice + 7;
         }
-        for (int NrOfVakje = 0; NrOfVakje < choice; NrOfVakje++){
-            vakje = vakje.getNextVakje();
-        }
+        vakje=vakje.getVakje(choice);
         return vakje;
     }
-    public void Distribute(int choice, Vakje vakje){
+
+    public void distribute(int choice, Vakje vakje){
         Vakje keuzevak = chooseVakje(choice, vakje);
         int nrOfBallsToDistribute = keuzevak.getNrOfBalls();
         keuzevak.setNrOfBalls(0);
@@ -42,24 +54,16 @@ public class Speler{
                 nrOfBallsToDistribute--;
                 Stelen(keuzevak);
             }
-            else{
-                if (keuzevak.getKalaha() == true && keuzevak.getSpeler()!=this){
-                }
-                else{
-                    previousNrOfBalls++;
-                    keuzevak.setNrOfBalls(previousNrOfBalls);
-                    nrOfBallsToDistribute--;
-                    if (keuzevak.getKalaha() == true && keuzevak.getSpeler()==this){
-                        toSwitchOrNotToSwitch(true, false);
-                    }
-                    else{
-                        toSwitchOrNotToSwitch(false, true);
-                    }
-                }
+            else if (keuzevak.getKalaha() == false || keuzevak.getSpeler()==this){
+                previousNrOfBalls++;
+                keuzevak.setNrOfBalls(previousNrOfBalls);
+                nrOfBallsToDistribute--;
+                switchBasedOnKalaha(keuzevak.getKalaha(), keuzevak.getSpeler());
             }
         }
         printScores(vakje);
     }
+
     public void Stelen(Vakje keuzevak){
         int counter = 0;
         toSwitchOrNotToSwitch(false, true);
@@ -77,10 +81,12 @@ public class Speler{
         keuzevak.setNrOfBalls(0);
         ThisKalaha.addNrOfBalls(NrOfBallsToAddToKalaha);
     }
+
     public void toSwitchOrNotToSwitch(boolean boolSp1, boolean boolSp2){
         this.aanDeBeurt = boolSp1;
         this.tegenspeler.aanDeBeurt = boolSp2;
     }
+
     public void printScores(Vakje vakje){
         int ballsSpeler = vakje.ballsOnPlayersSide(this);
         int ballsTegenspeler = vakje.ballsOnPlayersSide(this.getTegenspeler());
@@ -88,17 +94,11 @@ public class Speler{
         int ballInKalahaTegenspeler = vakje.ballsInKalaha(this.getTegenspeler());
         int scoreSpeler = ballsSpeler + ballInKalahaSpeler;
         int scoreTegenspeler = ballsTegenspeler + ballInKalahaTegenspeler;
+        System.out.println("Jouw score is " + scoreSpeler);
+        System.out.println("De score van jouw tegenspeler is " + scoreTegenspeler);
         if (ballsSpeler * ballsTegenspeler == 0){
             System.out.println("Er kan niet meer gespeeld worden. Het spel eindigt.");
-            if (scoreSpeler == scoreTegenspeler){
-                System.out.println("Het is gelijkspel.");
-            }
-            else if (scoreSpeler > scoreTegenspeler){
-                System.out.println("Jij hebt gewonnen.");
-            }
-            else {
-                System.out.println("Jouw tegenspeler heeft gewonnen.");
-            }
+            printWinner(scoreSpeler, scoreTegenspeler);
         }
     }
 
@@ -106,7 +106,32 @@ public class Speler{
         int score = this.eersteVakje.ballsOnPlayersSide(this) + this.eersteVakje.getVakje(6).ballsInKalaha(this);
         return score;
     }
+
     public void setEersteVakje(Vakje vak) {
         this.eersteVakje=vak;
     }
+
+    public void switchBasedOnKalaha(boolean isItAnKalaha, Speler player){
+        if (isItAnKalaha == true && player == this){
+            toSwitchOrNotToSwitch(true, false);
+        }
+        else{
+            toSwitchOrNotToSwitch(false, true);
+        }
+    }
+
+    public void printWinner(int JouwScore, int AndermansScore){
+        if (JouwScore == AndermansScore){
+            System.out.println("Het is gelijkspel.");
+        }
+        else if (JouwScore > AndermansScore){
+            System.out.println("Jij hebt gewonnen.");
+        }
+        else {
+            System.out.println("Jouw tegenspeler heeft gewonnen.");
+        }
+    }
+
+
+
 }
